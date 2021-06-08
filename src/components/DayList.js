f937386c40e8fd29d6baa6266dbbddc6c1cacddb
Matthew Thompson <thommati@outlook.com>
2021-06-08 13:11:28 -0600

    Give DayList component function the name DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import DayListItem from './DayListItem';
 
-export default function(props) {
+export default function DayList(props) {
   const dayListItems = props.days.map(d => {
     return <DayListItem
       key={d.id}
