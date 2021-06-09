6821c80ea9fc5391d576507bb2870d3e865e4f4b
Matthew Thompson <thommati@outlook.com>
2021-06-08 21:43:11 -0600

    Add Show and Header to Appointment; add to storybook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import './styles.scss';
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
-const Appointment = props => {
+import "./styles.scss";
+
+const Appointment = (props) => {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
   );
 };
 
