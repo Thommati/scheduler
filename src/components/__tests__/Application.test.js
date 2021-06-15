c21b61466bfad42957d91d35879605705cd8480b
Matthew Thompson <thommati@outlook.com>
2021-06-15 15:11:10 -0600

    Add test for deleting an appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -11,6 +11,8 @@ import {
   getByPlaceholderText,
   waitForElementToBeRemoved,
   queryByText,
+  prettyDOM,
+  getByDisplayValue
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -41,5 +43,36 @@ describe("Application", () => {
 
     const day = getAllByTestId(container, "day").find((d) => queryByText(d, "Monday"));
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+  });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render component
+    const { container } = render(<Application />);
+
+    // 2. Wait for data to be loaded by waiting for the text "Archie Cohen" to be displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointment = getAllByTestId(container, "appointment").find((appointment) =>
+      queryByText(appointment, "Archie Cohen")
+    );
+
+    // 3. Click the delete button for the booked appointment
+    fireEvent.click(getByAltText(appointment, "Delete"));
+
+    // 4. Check that confirmation message is shown
+    expect(getByText(appointment, "Are you sure you would like to delete?")).toBeInTheDocument();
+
+    // 5. Click on confirm button
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    // 6. Wait for "Deleting" to be displayed
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+    
+    // 7. Wait for add button to be displayed
+    await waitForElementToBeRemoved(() => getByText(appointment, "Deleting"));
+    expect(getByAltText(appointment, "Add")).toBeInTheDocument();
+
+    // 8. Check days remaining for Monday has the text "2 spots remaining"
+    const day = getAllByTestId(container, "day").find((d) => queryByText(d, "Monday"));
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
 });
