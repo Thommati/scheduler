dd3d72e28514734940ea1c70ea755811c2ff5424
Matthew Thompson <thommati@outlook.com>
2021-06-16 15:22:48 -0600

    Add cyprus test for deleting / cancelling an appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -20,7 +20,7 @@ describe('Appointments', () => {
     cy.contains('.appointment__card--show', 'Sylvia Palmer');
   });
 
-  it.only('should edit an interview', () => {
+  it('should edit an interview', () => {
     cy.get('[alt=Edit]').first().click({ force: true });
 
     cy.get('[data-testid=student-name-input]').clear().type('Lydia Miller-Jones');
@@ -30,5 +30,15 @@ describe('Appointments', () => {
 
     cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
     cy.contains('.appointment__card--show', 'Tori Malcolm');
+  });
+
+  it('should cancel an interview', () => {
+    cy.get('[alt=Delete]').first().click({ force: true });
+    cy.contains('Confirm').click();
+
+    cy.contains('Deleting').should('exist');
+    cy.contains('Deleting').should('not.exist');
+
+    cy.contains('.appointment__card--show', 'Archie Cohen').should('not.exist');
   });
 });
